b57c5ad5d5a61c0b302889e752d0a996e64c7a17
Amit Pandey <amit.p@icreon.com>
2025-02-21 13:52:14 +0530

    added new code

--- Common/DockerLearning.Common.Api/Logger/LoggerExtensions.cs
+++ Common/DockerLearning.Common.Api/Logger/LoggerExtensions.cs
@@ -3,10 +3,15 @@
     public static class LoggerExtensions
     {
         public static void LogTrace(this NLog.ILogger logger, LogOptions options) => LogHelper.Log(options.WithLogLevel(NLog.LogLevel.Trace));
+
         public static void LogDebug(this NLog.ILogger logger, LogOptions options) => LogHelper.Log(options.WithLogLevel(NLog.LogLevel.Debug));
+
         public static void LogInfo(this NLog.ILogger logger, LogOptions options) => LogHelper.Log(options.WithLogLevel(NLog.LogLevel.Info));
+
         public static void LogWarn(this NLog.ILogger logger, LogOptions options) => LogHelper.Log(options.WithLogLevel(NLog.LogLevel.Warn));
+
         public static void LogError(this NLog.ILogger logger, LogOptions options) => LogHelper.Log(options.WithLogLevel(NLog.LogLevel.Error));
+
         public static void LogFatal(this NLog.ILogger logger, LogOptions options) => LogHelper.Log(options.WithLogLevel(NLog.LogLevel.Fatal));
     }
 }
